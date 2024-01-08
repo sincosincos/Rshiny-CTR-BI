97ad8032152e5b8d8e681b7b83c102974137f3b2
HILMY ABDUL AZIZ <127839447+sincosincos@users.noreply.github.com>
2024-01-08 19:13:35 +0700

    Update app.R

--- app.R
+++ app.R
@@ -6,13 +6,8 @@ library(DT)
 library(dplyr)
 library(shinydashboard)
 
-# Data frame for CTRs
-CTR_data <- data.frame(
-  Day = 1:10,
-  `Left Sidebar` = c(2.5, 2.7, 2.8, 2.6, 3, 2.4, 2.9, 2.5, 2.6, 2.7),
-  `Center Page` = c(3.8, 3.5, 4, 3.7, 3.9, 3.6, 4.1, 3.4, 3.8, 3.9),
-  `Right Sidebar` = c(3.1, 2.9, 3, 3.2, 3.3, 2.8, 3.4, 3.1, 3.2, 3.5)
-)
+# Data CTRs
+CTR_data <- read.csv(".../CTR.csv", sep = ";")
 
 # Melt the data for visualization
 CTR_data_long <- reshape2::melt(CTR_data, id.vars = "Day", variable.name = "Ad_Placement", value.name = "CTR")
